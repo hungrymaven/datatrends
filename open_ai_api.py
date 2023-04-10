031e7d816f7efc60e11d90464672cb85551ee608
hungrymaven <89184598+hungrymaven@users.noreply.github.com>
2023-04-10 14:24:41 +0200

    Update on open_ai_api

--- open_ai_api.py
+++ open_ai_api.py
@@ -1,7 +1,23 @@
 import requests
 
-url = "https://api.openai.com/v1/endpoint"
-headers = {"Authorization": "Bearer <your_api_key>"}
+# Set up API endpoint and headers
+url = "https://api.openai.com/v1/analyze_data"
+headers = {
+    "Content-Type": "text/csv",
+    "Authorization": f"Bearer {API_KEY}"
+}
 
-response = requests.get(url, headers=headers)
+# Open and read the CSV file
+with open('example.csv', 'rb') as file:
+    csv_content = file.read()
 
+# Send the file to the OpenAI API
+response = requests.post(url, headers=headers, data=csv_content)
+
+# Process the response
+if response.status_code == 200:
+    # Success! Process the response here
+    print(response.json())
+else:
+    # Error handling here
+    print(f"Error {response.status_code}: {response.text}")
